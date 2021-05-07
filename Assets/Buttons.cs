5031b5cdf226eba7c7d8c1c9bbfb84a31f9b1680
Owen Hooper <owenhooper@gmail.com>
2021-05-07 14:31:35 -0400

    inital commit

---
+++ Assets/Buttons.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    public Text instructionsText;
+    public Button startButton;
+    public Button instructionsButton;
+    public Button quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(string name)
+    {
+        if (name == "StartButton")
+        {
+            LoadMain();
+        }
+        else if (name == "InstructionsButton")
+        {
+            GetInstructions();
+        }
+        else if (name == "QuitButton")
+        {
+            QuitGame();
+        }
+    }
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("HelloCardboard");
+    }
+
+    public void GetInstructions()
+    {
+        startButton.enabled = false;
+        instructionsButton.enabled = false;
+        quitButton.enabled = false;
+        instructionsText.enabled = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+ 
+
+
+    
+}
---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.4.1/Hello Cardboard/Scripts/ButtonController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ButtonController : MonoBehaviour
+{
+
+    public enum button
+    {
+        start,
+        instructions,
+        quit,
+        back
+    }
+
+    public GameObject instructionsCanvas;
+
+    public button chosenButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter()
+    {
+        GetComponent<Image>().color = Color.gray;
+    }
+
+
+    public void OnPointerExit()
+    {
+        GetComponent<Image>().color = Color.white;
+
+    }
+
+    public void OnPointerClick()
+    {
+        switch(chosenButton)
+        {
+            case button.start:
+                LoadMain();
+                break;
+            case button.instructions:
+                instructionsCanvas.SetActive(true);
+                break;
+            case button.quit:
+                QuitGame();
+                break;
+            case button.back:
+                instructionsCanvas.SetActive(false);
+                break;
+               
+        }
+
+
+    }
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
